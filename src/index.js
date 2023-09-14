e76e9c4a567fab5bc5571e1a0123a247bb521d4c
Airton de Sousa Martins Filho <ayrtonfilho12345@gmail.com>
2023-09-14 00:38:31 -0300

    new version

--- src/index.js
+++ src/index.js
@@ -1,40 +1,30 @@
 const Express = require("express");
-const BodyParser = require("body-parser");
 const Speakeasy = require("speakeasy");
 const qrcode = require('qrcode-terminal');
+const validateToken = require("./middlewares/validateToken");
 
 let app = Express();
+const secret = Speakeasy.generateSecret({ length: 20 });
 
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
+app.use(Express.json()); // Configuração para analisar o corpo JSON da solicitação
 
-// Essa é a secret que deve ser guardada com segurança e vinculada ao usuário.
-let secret = Speakeasy.generateSecret({ length: 20 });
-
-// Requisição para saber se o código de 6 dígitos é valido ou inválido.
-app.post("/validate", (request, response, next) => {
-    response.send({
-        "valid": Speakeasy.totp.verify({
-            secret: secret.base32,
-            encoding: "base32",
-            token: request.body.code,
-            window: 0
-        })
-    });
+app.use("/validate", (req, res) => {
+    validateToken(secret, req, res); // Passa a secret como argumento
 });
 
-// Gerando uma URL para o QR code
 const otpauthUrl = Speakeasy.otpauthURL({
     secret: secret.base32,
     encoding: "base32",
-    label: '@USER_airtonfilho',
-    issuer: 'TWO-FACTOR-AUTH',
+    label: "@USER_airtonfilho",
+    issuer: "TWO-FACTOR-AUTH",
 });
 
-qrcode.generate(otpauthUrl, { small: true }, function (qrcodeText) {
-    console.log(qrcodeText); // o QR Code irá ser mostrado no terminal.
+qrcode.generate(otpauthUrl, { small: true }, (qrcodeText) => {
+    console.log(qrcodeText); // O QR Code será exibido no terminal.
 });
 
-app.listen(3000, () => {
-    console.log("Listening at 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`);
 });
